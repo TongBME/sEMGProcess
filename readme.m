8eaa28e9d9e6e7fa851a835fbc78f49e6465e705
Tong <30516070+TongBME@users.noreply.github.com>
2020-04-13 15:37:15 +0800

    Update and rename readme.txt to readme.m

--- readme.m
+++ readme.m
@@ -1,3 +1,4 @@
-# sEMG data preprocess and feature extraction
+# Hellow
+sEMG data preprocess and feature extraction
 # preprocess includes IIR filtering.
 # feature extraction includes time dormain processing and frequence dormain processing.
